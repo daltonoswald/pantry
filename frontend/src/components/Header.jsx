8e94ac87ff97d4f9b9ab89a9d99a10185a51bf9b
daltonoswald <daltonoswald@gmail.com>
2026-07-21 12:12:40 -0400

    navbar redesign started

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Col, Container, Nav, Navbar, NavDropdown, Row, Form, Button } from 'react-bootstrap'
-import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import { Link, NavLink, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NewPantryItem from './modals/NewPantryItem';
 import { Search } from 'react-bootstrap-icons';
@@ -31,7 +31,6 @@ export default function Header() {
     return (
         <>
         <Navbar sticky='top' expand='lg' className='pantry-nav d-flex justify-content-around' >
-            {/* <Container fluid className='pantry-primary mx-2'> */}
                 <Row>
                     <Navbar.Brand as={Link} to='/'>Pantry</Navbar.Brand>
                 </Row>
@@ -101,7 +100,54 @@ export default function Header() {
                         </Col>
                     )}
                 </Row>
-            {/* </Container> */}
+        </Navbar>
+        <Navbar sticky='top' expand='lg' className='pantry-nav d-flex justify-content-around' >
+            <div className='navbar-left'>
+                <NavLink to='/' style={({ isActive }) => 
+                    isActive ? { color: '#C0563E'} : { color: 'black'}}>Pantry</NavLink>
+                <NavLink to='/search' className={({ isActive, isPending }) => 
+                    isPending ? "pending"  : isActive ? "active" : "" }>Search</NavLink>
+            </div>
+            <div className='navbar-right'>
+                <Form className='d-flex' onSubmit={handleNavSearch}>
+                    <Form.Control
+                        type='search'
+                        name='query'
+                        placeholder='Search...'
+                        className='me-2'
+                        defaultValue={query || ''}
+                        aria-label='Search'
+                    />
+                    <Button type='submit' className='pantry-secondary'>
+                        <Search color='white' />
+                    </Button>
+                </Form>
+                    {!token && (
+                            <Col>
+                                <Col>
+                                    <Nav.Item>
+                                        <Nav.Link as={Link} to='/login'>Log in</Nav.Link>
+                                    </Nav.Item>
+                                </Col>
+                                <Col>
+                                    <Nav.Item>
+                                        <Nav.Link as={Link} to='/sign-up'>Sign up</Nav.Link>
+                                    </Nav.Item>
+                                </Col>
+                            </Col>
+                    )}
+                    {token && (
+                        <Col md='auto' className='d-flex align-items-center'>
+                            <NavDropdown title={username} className='pantry-heading' >
+                                <NavDropdown.Item as={Link} to={`/user/${username}`} className='pantry-heading'>{username}</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to={'/search'} reloadDocument className='pantry-heading'>Search</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to={'/new-recipe'} className='pantry-heading'>New Recipe</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleOpenNewPantryItem} className='pantry-heading'>New Pantry Item</NavDropdown.Item>
+                                <NavDropdown.Item onClick={logout} className='pantry-heading'>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        </Col>
+                    )}
+            </div>
         </Navbar>
         <NewPantryItem openNewPantryItem={openNewPantryItem} setOpenNewPantryItem={setOpenNewPantryItem} />
         </>
