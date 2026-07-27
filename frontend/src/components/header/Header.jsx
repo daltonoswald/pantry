46a1c62efc51e5cc43b9311ccc076cef5f32a9a8
daltonoswald <daltonoswald@gmail.com>
2026-07-27 12:40:42 -0400

    redoing navbar search

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,8 +4,9 @@ import { Link, NavLink, useLocation, useNavigate, useSearchParams } from 'react-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './header.styles.css'
 import NewPantryItem from '../modals/NewPantryItem';
-import { MdAccountCircle } from 'react-icons/md'
+import { MdAccountCircle, MdOutlineSearch } from 'react-icons/md'
 import { Search, PersonCircle } from 'react-bootstrap-icons';
+import { FaSearch } from 'react-icons/fa';
 
 export default function Header() {
     const [openNewPantryItem, setOpenNewPantryItem] = useState(false);
@@ -32,73 +33,6 @@ export default function Header() {
 
     return (
         <>
-        {/* <Navbar sticky='top' expand='lg' className='pantry-nav d-flex justify-content-around' >
-                <Row>
-                    <Navbar.Brand as={Link} to='/'>Pantry</Navbar.Brand>
-                </Row>
-                <Row>
-                    <Col>
-                        <Nav.Item>
-                            <Nav.Link as={Link} to='/'>Recipes</Nav.Link>
-                        </Nav.Item>
-                    </Col>
-                    <Col>
-                        <Nav.Item>
-                            <Nav.Link as={Link} to='/search'>Search</Nav.Link>
-                        </Nav.Item>
-                    </Col>
-                    <Col>
-                        <Nav.Item>
-                            <Nav.Link as={Link} to='/about'>About</Nav.Link>
-                        </Nav.Item>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form className='d-flex' onSubmit={handleNavSearch}>
-                            <Form.Control
-                                type='search'
-                                name='query'
-                                placeholder='Search...'
-                                className='me-2'
-                                defaultValue={query || ''}
-                                aria-label='Search'
-                            />
-                            <Button type='submit' className='pantry-secondary'>
-                                <Search color='white' />
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-                <Row className='text-light pantry-heading'>
-                    {!token && (
-                        <Col>
-                            <Col>
-                                <Nav.Item>
-                                    <Nav.Link as={Link} to='/login'>Log in</Nav.Link>
-                                </Nav.Item>
-                            </Col>
-                            <Col>
-                                <Nav.Item>
-                                    <Nav.Link as={Link} to='/sign-up'>Sign up</Nav.Link>
-                                </Nav.Item>
-                            </Col>
-                        </Col>
-                    )}
-                    {token && (
-                        <Col md='auto' className='d-flex align-items-center'>
-                            <NavDropdown title={username} className='pantry-heading' >
-                                <NavDropdown.Item as={Link} to={`/user/${username}`} className='pantry-heading'>{username}</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to={'/search'} reloadDocument className='pantry-heading'>Search</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to={'/new-recipe'} className='pantry-heading'>New Recipe</NavDropdown.Item>
-                                <NavDropdown.Item onClick={handleOpenNewPantryItem} className='pantry-heading'>New Pantry Item</NavDropdown.Item>
-                                <NavDropdown.Item onClick={logout} className='pantry-heading'>Logout</NavDropdown.Item>
-                            </NavDropdown>
-                        </Col>
-                    )}
-                </Row>
-        </Navbar> */}
-        {/* New ^^ Old vv */}
         <Navbar sticky='top' expand='lg' className='pantry-nav' >
             <div className='navbar-left'>
                 <NavLink to='/' style={({ isActive }) => isActive ? { color: '#C0563E'} : { color: 'black'}} className='navbar-brand'>
@@ -115,7 +49,7 @@ export default function Header() {
                 </NavLink>
             </div>
             <div className='navbar-right'>
-                <Form className='d-flex' onSubmit={handleNavSearch}>
+                {/* <Form className='d-flex' onSubmit={handleNavSearch}>
                     <Form.Control
                         type='search'
                         name='query'
@@ -125,26 +59,27 @@ export default function Header() {
                         aria-label='Search'
                     />
                     <Button type='submit' className='pantry-secondary'>
-                        <Search color='white' />
+                        <MdOutlineSearch color='black' />
                     </Button>
-                </Form>
+                </Form> */}
+                <form className='pantry-nav-search' onSubmit={handleNavSearch}>
+                    <MdOutlineSearch color='black' />
+                    <input  
+                        type='text'
+                        name='query'
+                        placeholder='Search...'
+                        defaultValue={query || ''}
+                        aria-label='Search'
+                    />
+                </form>
                     {!token && (
-                            <Col>
-                                <Col>
-                                    <Nav.Item>
-                                        <Nav.Link as={Link} to='/login'>Log in</Nav.Link>
-                                    </Nav.Item>
-                                </Col>
-                                <Col>
-                                    <Nav.Item>
-                                        <Nav.Link as={Link} to='/sign-up'>Sign up</Nav.Link>
-                                    </Nav.Item>
-                                </Col>
-                            </Col>
+                            <div className='pantry-nav-unauthenticated'>
+                                <Link to={'/login'}>Log in</Link>
+                                <Link to={'/sign-up'}>Sign up</Link>
+                            </div>
                     )}
                     {token && (
                         <Col md='auto' className='d-flex align-items-center pantry-heading-user'>
-                            {/* <PersonCircle color='black' className='pantry-heading-profile-icon' onClick={() => navigate(`/user/${username}`)} /> */}
                             <MdAccountCircle size='1.5rem' color='black' className='pantry-heading-profile-icon' onClick={() => navigate(`/user/${username}`)} />
                             <NavDropdown title={username} className='pantry-heading' >
                                 <NavDropdown.Item as={Link} to={`/user/${username}`} className='pantry-heading'>{username}</NavDropdown.Item>
