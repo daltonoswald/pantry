c57c3b8a25bdf99eef43d443b78aea6b92b317ea
daltonoswald <daltonoswald@gmail.com>
2026-09-09 12:27:06 -0400

    new recipe page reworked without bootstrap

--- frontend/src/pages/newRecipe/NewRecipe.jsx
+++ frontend/src/pages/newRecipe/NewRecipe.jsx
@@ -1,11 +1,12 @@
-import { Container, Row, Col, Form, FloatingLabel, Button, InputGroup, Alert } from 'react-bootstrap';
 import Header from '../../components/header/Header';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { Trash, PlusCircle, X } from 'react-bootstrap-icons';
+import { GoPlusCircle } from 'react-icons/go';
+import { MdClose } from 'react-icons/md';
 import TinyEditor from '../../components/tinyEditor/TinyEditor';
 import ImageUpload from '../../components/ImageUpload';
+import Alert from '../../components/modals/Alert';
 
 export default function NewRecipe() {
     const navigate = useNavigate();
@@ -118,7 +119,7 @@ export default function NewRecipe() {
     return (
         <div className='app'>
             <Header />
-            <Container className='my-auto pb-3' fluid>
+            {/* <Container className='my-auto pb-3' fluid>
                 <Form className='w-50 p-3 mx-auto' onSubmit={handleSubmit}>
                     <Row>
                         <Col className='m-2'>
@@ -199,9 +200,6 @@ export default function NewRecipe() {
                                         />
                                 </Form.Group>
                                 <Col xs={1} className='me-2 w-auto'>
-                                    {/* <Button variant='danger' type='button' onClick={() => handleRemoveIngredient(index)}>
-                                        <Trash color='black' />
-                                    </Button> */}
                                     <X className='remove-button' onClick={() => handleRemoveIngredient(index)} />
                                 </Col>
                             </Form.Group>
@@ -226,18 +224,12 @@ export default function NewRecipe() {
                                         />
                                     </Col>
                                     <Col xs='auto' className='me-2'>
-                                        {/* <Button variant='danger' type='button' onClick={() => handleRemoveStep(index)}>
-                                            <Trash color='black' />
-                                        </Button> */}
                                         <X className='remove-button' onClick={() => handleRemoveStep(index)} />
                                     </Col>
                                 </Form.Group>
                             ))}
                         <Row>
                             <Col className='my-4'>
-                                {/* <FloatingLabel controlId='formTags' label='Tags (separate with commas)'>
-                                <Form.Control name='tags' type='text' placeholder='Tags (separate with commas)' />
-                                </FloatingLabel> */}
                             <Form.Group className='my-4' controlId='tags'>
                                 <Form.Label>TAGS (seperate with commas)</Form.Label>
                                 <Form.Control placeholder='breakfast, lunch, dinner' required />
@@ -251,7 +243,133 @@ export default function NewRecipe() {
                 {message && (
                     <Alert className='w-25 m-3 p-3 mx-auto' variant='danger'>{message}</Alert>
                 )}
-            </Container>
+            </Container> */}
+            <div className='new-recipe-container'>
+                <form className='new-recipe-form' onSubmit={handleSubmit}>
+                    <div className='form-group'>
+                        <ImageUpload onUpload={(url) => {
+                            console.log('Setting imageUrl: ', url)
+                            setImageUrl(url)}
+                        } />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='title' className='form-label'>Recipe Title</label>
+                        <input 
+                            type='text'
+                            id='title'
+                            name='title'
+                            placeholder='e.g. Sourdough Pizza'
+                            className='form-input'
+                            required />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='description' className='form-label'>Description</label>
+                        <input 
+                            type='text'
+                            id='description'
+                            name='description'
+                            placeholder='Share a quick description of your creation'
+                            className='form-input'
+                            required />
+                    </div>
+                    <div className='new-recipe-block-container'>
+                        <div className='new-recipe-block'>
+                            <label htmlFor='cookTime' className='form-block-label'>Cook (mins)</label>
+                            <input 
+                                type='number'
+                                id='cookTime'
+                                name='cookTime'
+                                defaultValue={25}
+                                className='form-block-input'
+                                required />
+                        </div>
+                        <div className='new-recipe-block'>
+                            <label htmlFor='servings' className='form-block-label'>Servings</label>
+                            <input 
+                                type='number'
+                                id='servings'
+                                name='servings'
+                                defaultValue={4}
+                                className='form-block-input'
+                                required />
+                        </div>
+                    </div>
+                    <div className='form-ingredient-container'>
+                        <h2 className='form-title'>Ingredients</h2>
+                        <p className='add-ingredient' onClick={handleAddIngredient}><GoPlusCircle /> Add Ingredient</p>
+                    </div>
+                    {ingredientList.map((ingredient, index) => (
+                        <div className='form-group-ingredient'>
+                            <input
+                                type='number'
+                                name='unitAmount'
+                                id='unitAmount'
+                                step='0.25'
+                                onChange={(e) => handleUnitAmountChange(e, index)}
+                                min={0}
+                                defaultValue={0}
+                                required />
+                            <select onChange={(e) => handleUnitChange(e, index)} name='unit' required>
+                                {cookingUnits.map((unit) => 
+                                    <option
+                                        key={unit}
+                                        name='unit'
+                                        defaultValue={'tsp'}
+                                        value={unit}
+                                        required
+                                        >{unit}</option>
+                                )}
+                            </select>
+                            <input
+                                type='text'
+                                name='ingredientNote'
+                                id='ingredientNote'
+                                onChange={(e) => handleIngredientNoteChange(e, index)}
+                                placeholder='Notes (dcied, thin-sliced, etc.)'
+                                required />
+                            <input
+                                type='text'
+                                name='ingredient'
+                                id='ingredient'
+                                onChange={(e) => handleIngredientChange(e, index)}
+                                placeholder='Ingredient name...'
+                                required />
+                            {/* <p onClick={() => handleRemoveIngredient(index)}>&times;</p> */}
+                            <MdClose onClick={() => handleRemoveIngredient(index)} className='form-ingredient-delete' />
+                        </div>
+                    ))}
+                    <div className='form-method-container'>
+                        <h2 className='form-title'>Method</h2>
+                        <p className='add-ingredient' onClick={handleAddIngredient}><GoPlusCircle /> Add Step</p>
+                        {steps.map((step, index) => (
+                            <div className='form-group-step'>
+                                <p className='recipe-step-counter'>{index + 1}</p>
+                                <input
+                                    type='textarea'
+                                    rows={2}
+                                    value={step}
+                                    onChange={(e) => handleStepChange(e, index)}
+                                    placeholder={`Step ${index + 1}`}
+                                    required />
+                                <MdClose onClick={() => handleRemoveStep(index)} className='form-step-delete' />
+                            </div>
+                        ))}
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='tags' className='form-label'>Tags (seperate with commas)</label>
+                        <input 
+                            type='text'
+                            id='tags'
+                            name='tags'
+                            placeholder='breakfast, lunch, dinner'
+                            className='form-input'
+                            required />
+                    </div>
+                    {message && (
+                        <Alert message={message} />
+                    )}
+                </form>
+            </div>
         </div>
     )
 }
